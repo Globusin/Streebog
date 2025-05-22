3455c5f83a781e76a2efad5a1af66310e1629055
Globusin <karpov.globik@yandex.ru>
2025-05-22 21:31:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using ConsoleApp1.Stribog;
+using System.Diagnostics;
+using System.Text;
+
+//var strib = new Streebog();
+
+//int bufferSize = 1 * 1024 * 1024;
+//byte[] data = new byte[bufferSize];
+//new Random().NextBytes(data);
+
+//var stopwatch = Stopwatch.StartNew();
+//var hash = strib.H(data, 64);
+//stopwatch.Stop();
+
+//double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+//double speedMBperSecond = (bufferSize / (1024.0 * 1024.0)) / elapsedSeconds;
+
+//Console.WriteLine($"Размер данных: {bufferSize / (1024 * 1024)} MB");
+//Console.WriteLine($"Время: {elapsedSeconds:F2} сек");
+//Console.WriteLine($"Скорость: {speedMBperSecond:F2} MB/s");
+//Console.WriteLine($"Хеш: {Convert.ToHexString(hash).ToLower()}");
+//return;
+
+class Program
+{
+    private static Random random = new Random();
+    private static Streebog strib = new Streebog();
+
+    static string GenerateRandomMessage()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        int length = random.Next(10, 50);
+
+        var message = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            message[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(message);
+    }
+
+    static (string, string) FindCollisionBasic(int n)
+    {
+        var hashes = new Dictionary<string, string>();
+        while (true)
+        {
+            string msg = GenerateRandomMessage();
+            byte[] hash = strib.H(Encoding.UTF8.GetBytes(msg), n);
+
+            string hexHash = BitConverter.ToString(hash).ToLower();
+
+            if (hashes.TryGetValue(hexHash, out string existingMsg))
+            {
+                return (existingMsg, msg);
+            }
+
+            hashes[hexHash] = msg;
+        }
+    }
+
+    static byte[] GenerateRandomBytes(int length)
+    {
+        byte[] bytes = new byte[length];
+        random.NextBytes(bytes);
+        return bytes;
+    }
+
+    static (byte[], byte[]) FindCollisionIterative(int n)
+    {
+        byte[] x0 = GenerateRandomBytes(n + 1);
+        byte[] x = x0;
+        byte[] x_prime = x0;
+
+        int i = 0;
+        while (true)
+        {
+            x = strib.H(x, n);
+            x_prime = strib.H(strib.H(x_prime, n), n);
+
+            if (ByteArraysEqual(x, x_prime))
+                break;
+
+            i++;
+        }
+
+        x_prime = x;
+        x = x0;
+        for (int j = 0; j <= i; j++)
+        {
+            byte[] h_x = strib.H(x, n);
+            byte[] h_x_prime = strib.H(x_prime, n);
+
+            if (ByteArraysEqual(h_x, h_x_prime))
+            {
+                if (!ByteArraysEqual(x, x_prime))
+                {
+                    return (x, x_prime);
+                }
+            }
+
+            x = h_x;
+            x_prime = h_x_prime;
+        }
+
+        throw new Exception("Collision not found");
+    }
+
+    static bool ByteArraysEqual(byte[] a, byte[] b)
+    {
+        if (a.Length != b.Length)
+            return false;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        //var (msg1, msg2) = FindCollisionBasic(3);
+        //Console.WriteLine($"Collision found:\nMessage 1: {msg1}\nMessage 2: {msg2}");
+
+        //var (msg1, msg2) = FindCollisionIterative(2);
+        //Console.WriteLine($"Collision found:\nMessage 1: {BitConverter.ToString(msg1).ToLower()}\nMessage 2: {BitConverter.ToString(msg2).ToLower()}");
+    }
+}
---
+++ Stribog/Streebog.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1.Stribog;
+
+public class Streebog
+{
+    private byte[] N;
+    private List<List<byte[]>> tables;
+    private byte[] tempBytes;
+
+    public Streebog()
+    {
+        tables = new List<List<byte[]>>();
+        for (int i = 0; i < 64; i++)
+        {
+            string jsonContent = File.ReadAllText($"T_{i}.json");
+            var table = JsonConvert.DeserializeObject<List<byte[]>>(jsonContent);
+            tables.Add(table);
+        }
+
+        tempBytes = new byte[64];
+    }
+
+    public byte[] H(byte[] M, int n = 64)
+    {
+        N = new byte[64];
+        byte[] h = new byte[64];
+        byte[] m = new byte[64];
+        byte[] sigma = new byte[64];
+
+        var l = M.Length;
+        while (l > 64)
+        {
+            Array.Copy(M, l - 64, m, 0, 64);
+            h = g(h, m, N);
+            N = AddMod512(N, new byte[64]);
+            sigma = AddMod512(sigma, m);
+            Array.Copy(M, M, 64);
+            l -= 64;
+        }
+
+        if (l != 64)
+        {
+            Array.Copy(new byte[64], m, 64 - l - 1);
+            m[64 - l - 1] = 1;
+            Array.Copy(M, 0, m, 64 - l, l);
+        }
+        else
+            Array.Copy(M, m, 64);
+
+        h = g(N, h, m);
+        byte[] lengthInBytes = new byte[64];
+        BitConverter.GetBytes(M.Length * 8).CopyTo(lengthInBytes, 0);
+        Array.Reverse(lengthInBytes);
+        N = AddMod512(N, lengthInBytes);
+        sigma = AddMod512(sigma, m);
+
+        var str = Convert.ToHexString(N).ToLower();
+
+        h = g(new byte[64], h, N);
+        h = g(new byte[64], h, sigma);
+
+        var result = new byte[n];
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = h[i];
+        }
+        return result;
+    }
+
+    private byte[] AddMod512(byte[] a, byte[] b)
+    {
+        int temp = 0;
+        for (int i = 63; i >= 0; i--)
+        {
+            temp = a[i] + b[i] + (temp >> 8);
+            tempBytes[i] = (byte)(temp & 0xFF);
+        }
+        return tempBytes;
+    }
+
+    private byte[] g(byte[] N, byte[] h, byte[] m)
+    {
+        return X(X(E(LPS(X(h, N)), m), h), m);
+    }
+
+    private byte[] E(byte[] k, byte[] m)
+    {
+        var result = X(k, m);
+
+        for (var i = 0; i < 12; i++)
+        {
+            result = LPS(result);
+            k = LPS(X(k, Constants.CVector[i]));
+            result = X(result, k);
+        }
+
+        return result;
+    }
+
+    private byte[] X(byte[] a, byte[] b)
+    {
+        var c = new byte[64];
+        var intern = 0;
+        for (var i = 63; i >= 0; i--)
+        {
+            intern = a[i] + b[i] + (intern >> 8);
+            c[i] = (byte)intern;
+        }
+
+        return c;
+    }
+
+    private byte[] LPS(byte[] data)
+    {
+        var result = new byte[64];
+
+        for (int i = 0; i < 64; i++)
+        {
+            tempBytes = tables[i][data[i]];
+            for (int j = 0; j < 64; j++)
+            {
+                result[j] ^= tempBytes[j];
+            }
+        }
+
+        return result;
+    }
+}
